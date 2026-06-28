245c75095abf1a9f9ccf3bf4de083990aa3d2161
cHANGTEEZY <jagatgurung30@gmail.com>
2026-06-29 03:35:06 +0545

    complete user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,20 +1,22 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+const USERS = [
+  {
+    id: 1,
+    name: 'Sushank',
+  },
+  {
+    id: 2,
+    name: 'Amrita',
+  },
+];
 
 @Controller('user')
 export class UserController {
   @Get()
   getUsers(@Query('name') name: string) {
-    const USERS = [
-      {
-        id: 1,
-        name: 'Sushank',
-      },
-      {
-        id: 2,
-        name: 'Amrita',
-      },
-    ];
-
     if (name) {
       return USERS.filter((user) =>
         user.name.toLowerCase().includes(name.toLowerCase()),
@@ -22,5 +24,35 @@ export class UserController {
     }
 
     return USERS;
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    const userId = id;
+    const result = USERS.find((user) => String(user.id) === userId);
+
+    if (!result) {
+      return {
+        message: 'Resource with given id is not found',
+      };
+    }
+
+    return result;
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return {
+      data: createUserDto,
+      message: 'User Created Successfully',
+    };
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return {
+      data: { id, ...updateUserDto },
+      message: 'User Created Successfully',
+    };
   }
 }
